2a670e9a9843d7bf29d414d611a259d10113db90
“Anais <ananavelez@gmail.com>
2017-08-10 16:20:16 -0400

    totalmente listo

--- assets/js/javaScript.js
+++ assets/js/javaScript.js
@@ -1,4 +1,5 @@
-function frutas(){}
+function frutas(){
+
 	var valor= document.getElementById("lista").value;
 	var listas=document.getElementById("contenedor");
 	listas.innerHTML+=('<ul><li>'+valor+'</li></ul>');
